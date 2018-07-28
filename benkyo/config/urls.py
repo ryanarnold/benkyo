00a2e037e358bad5ce469597f6ca500979840902
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2018-07-28 17:27:14 +0800

    Add logout url.

--- benkyo/config/urls.py
+++ benkyo/config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
     path('authenticate-user/', views.authenticate_user, name='authenticate-user'),
 ]
