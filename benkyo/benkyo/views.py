9c0cd4d583f333d9f261e53d994ccfd5609f5416
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2018-07-28 17:24:40 +0800

    Add views for login.

--- benkyo/benkyo/views.py
+++ benkyo/benkyo/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as login_user
+from django.contrib.auth import logout as logout_user
+from django.contrib.auth import authenticate
+from django.urls import reverse
 
-# Create your views here.
+
+@login_required
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def authenticate_user(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(username=username, password=password)
+
+    if user:
+        login_user(request, user)
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+
+def logout(request):
+    logout_user(request)
+
+
+def register(request):
+    return render(request, 'register.html')
