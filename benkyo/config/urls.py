947662731ba5b55fdb998ba2f923141b550cca9c
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2018-07-28 17:08:56 +0800

    Add login and index urls.

--- benkyo/config/urls.py
+++ benkyo/config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from benkyo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
 ]
