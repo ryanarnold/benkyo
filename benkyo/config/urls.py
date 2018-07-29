9f029da4d05a505e39dcbc3baa6bf61bdd93ba5b
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2018-07-29 14:31:26 +0800

    Create urls for deck deletion.

--- benkyo/config/urls.py
+++ benkyo/config/urls.py
@@ -31,4 +31,8 @@ urlpatterns = [
     path('decks/', views.decks, name='decks'),
     path('decks/create/', views.decks_create, name='decks-create'),
     path('decks/create/successful/', views.decks_create_successful, name='decks-create-successful'),
+    
+    path('decks/<int:deck_id>/delete/confirm/', views.decks_delete_confirm, name='decks-delete-confirm'),
+    path('decks/<int:deck_id>/delete/', views.decks_delete_confirm, name='decks-delete'),
+    path('decks/delete/success', views.decks_delete_confirm, name='decks-delete-success'),
 ]
