5d97d0eeddb14a2e2101934be986b5f98002a769
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2018-08-11 15:03:24 +0800

    Refactor.

--- benkyo/static/js/review.js
+++ benkyo/static/js/review.js
@@ -2,9 +2,14 @@
 var currentIndex = 0;
 var secondsElapsed = 0;
 
+$question = $('#question');
+$inputAnswer = $('#input-answer');
+$timeElapsed = $('#time-elapsed');
+$btnSubmit = $('#btn-submit');
+
 function updateQuestion() {
     secondsElapsed = 0;
-    $('#question').html(reviewItems[currentIndex].question);
+    $question.html(reviewItems[currentIndex].question);
 }
 
 function finishReview() {
@@ -27,41 +32,41 @@ function finishReview() {
 
 function nextQuestion() {
     currentIndex += 1;
-    $('#question').removeClass('animated shake bounce faster');
+    $question.removeClass('animated shake bounce faster');
 
     if (currentIndex < reviewItems.length) {
         updateQuestion();
-        $('#input-answer').val('');
+        $inputAnswer.val('');
     } else {
     }
 }
 
 function checkAnswer() {
-    var answer = $('#input-answer').val();
+    var answer = $inputAnswer.val();
     var correctAnswer = reviewItems[currentIndex].answer;
 
     if (answer.toLowerCase() == correctAnswer.toLowerCase()) {
-        $('#question').addClass('animated bounce faster');
-        // alert('CORRECT!');
+        $question.addClass('animated bounce faster');
     } else {
-        $('#question').addClass('animated shake faster');
-        // alert('WRONG!');
+        $question.addClass('animated shake faster');
     }
 
     setTimeout(nextQuestion, 1000);
 }
 
-$('#btn-submit').mouseup(checkAnswer);
-$('#input-answer').keyup(function (event) {
+$btnSubmit.mouseup(checkAnswer);
+
+$inputAnswer.keyup(function (event) {
     if (event.which == 13) {
         checkAnswer();
     }
 });
+
 updateQuestion();
 
 function updateTimer() {
     secondsElapsed += 1;
-    $('#time-elapsed').text(secondsElapsed + ' secs');
+    $timeElapsed.text(secondsElapsed + ' secs');
 }
 
 updateTimer();
